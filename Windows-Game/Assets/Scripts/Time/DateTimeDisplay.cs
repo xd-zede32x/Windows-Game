f64d5e5a6bea40e7209e9f8e7380d5cf7649c75d
xd-zede32x <homekor24@gmail.com>
2024-07-18 18:17:12 +0300

    Сделал базовый интерфейс и написал скрикт на время года и время  :heavy_check_mark:

---
+++ Windows-Game/Assets/Scripts/Time/DateTimeDisplay.cs
@@ -0,0 +1,40 @@
+using TMPro;
+using System;
+using UnityEngine;
+
+namespace ScriptsTime
+{
+    public class DateTimeDisplay : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _timeText;
+        [SerializeField] private TextMeshProUGUI _dayText;
+
+        private void Awake()
+        {
+            if (_timeText == null || _dayText == null)
+            {
+                Debug.LogError("TimeText of DayText is not assigned");
+
+                return;
+            }
+
+            UpdateTime();
+            UpdateDay();
+
+            InvokeRepeating(nameof(UpdateTime), 1f, 1f);
+            InvokeRepeating(nameof(UpdateDay), 86400f, 86400f);
+        }
+
+        private void UpdateTime()
+        {
+            var time = DateTime.Now;
+            _timeText.text = $"{time.Hour}:{time.Minute}:{time.Second}";
+        }
+
+        private void UpdateDay()
+        {
+            var day = DateTime.Now;
+            _dayText.text = $"{day.Day}:{day.Month}:{day.Year}";
+        }
+    }
+}
